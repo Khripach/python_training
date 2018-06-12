be1c2c4df53f06214de71880668cf2ac35121475
Victor Khripach <anyudie5@mail.ru>
2018-06-12 15:27:39 +0400

    Задание №15: Параметризовать тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,9 +1,34 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(prefix, maxlen):
+    return prefix + "".join([random.choice(string.digits) for i in range(random.randrange(maxlen))])
+
+
+def random_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@mail.ru"
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), nickname=random_string("nickname", 10), company=random_string("company", 10), address=random_string("address", 10),
+            homephone=random_number("8", 10), mobilephone=random_number("7", 10), workphone=random_number("3", 10), secondaryphone=random_number("2", 10),
+            email=random_email("name", 5), byear=random_number("19", 2))
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="ivan", lastname="taranov", nickname="tara", company="book", address="lenina 1", homephone="555555", mobilephone="666666", workphone="777777", secondaryphone="888888", email="123@mail.ru", byear="1985")
     app.contact.add_new(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
