6382eff8c308984f0b88c09c04e9f9923609609f
Victor Khripach <anyudie5@mail.ru>
2018-06-15 19:34:41 +0400

    Задание №18: Реализовать генераторы тестовых данных Задание №19: Реализовать загрузку тестовых данных из файлов

---
+++ generator/contact.py
@@ -0,0 +1,49 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(prefix, maxlen):
+    return prefix + "".join([random.choice(string.digits) for i in range(random.randrange(maxlen))])
+
+
+def random_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@mail.ru"
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), nickname=random_string("nickname", 10), company=random_string("company", 10), address=random_string("address", 10),
+            homephone=random_number("8", 10), mobilephone=random_number("7", 10), workphone=random_number("3", 10), secondaryphone=random_number("2", 10),
+            email=random_email("name", 5), byear=random_number("19", 2))
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,33 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_number(prefix, maxlen):
-    return prefix + "".join([random.choice(string.digits) for i in range(random.randrange(maxlen))])
-
-
-def random_email(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@mail.ru"
-
-
-testdata = [
-    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), nickname=random_string("nickname", 10), company=random_string("company", 10), address=random_string("address", 10),
-            homephone=random_number("8", 10), mobilephone=random_number("7", 10), workphone=random_number("3", 10), secondaryphone=random_number("2", 10),
-            email=random_email("name", 5), byear=random_number("19", 2))
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add_new(contact)
     new_contacts = app.contact.get_contact_list()
